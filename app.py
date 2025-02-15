969f39dc4c952f5c819defb83411580b527717d3
Andrei Baraitaru <andrei.bara08@gmail.com>
2025-02-15 03:49:18 +0100

    backend version 1

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return {"message": "Career Recommendation API"}
+#w load trained model
+script_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(script_dir, "../recommender-models/career_recommender.pkl")
+model = joblib.load(model_path)
+
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    data = request.json  # Get JSON data from request
+    features = pd.DataFrame([data])  # Convert input to DataFrame
+    career = model.predict(features)[0]  # Make prediction
+    return jsonify({"recommended_career": career})  # Return response
 
 if __name__ == "__main__":
     app.run(debug=True)
