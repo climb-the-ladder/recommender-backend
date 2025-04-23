cf2aaf30746d642e85bc8f0262a9918d139ba1d6
Zaid AlTayan <zaltayan.ieu2022@student.ie.edu>
2025-04-23 23:11:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes.recommendations import recommendation
 from routes.chatbot import chatbot
+import os
 
 app = Flask(__name__)
 CORS(app) 
@@ -10,4 +11,5 @@ app.register_blueprint(recommendation)
 app.register_blueprint(chatbot)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
