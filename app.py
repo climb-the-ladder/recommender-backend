d38a8ffba224d2478f84b025dd46218e4d67a2f6
Zaid AlTayan <zaltayan.ieu2022@student.ie.edu>
2025-04-24 08:08:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from routes.chatbot import chatbot
 import os
 
 app = Flask(__name__)
-CORS(app) 
+
+# Configure CORS to allow requests from any origin
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"], "allow_headers": ["Content-Type"]}})
 
 app.register_blueprint(recommendation)
 app.register_blueprint(chatbot)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
